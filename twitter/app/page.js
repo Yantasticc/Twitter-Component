b1f2ccaa8431b81af157cf0e1c95b1c1f488aae5
Yantasticc <yashkalamkhede2002@gmail.com>
2023-11-01 20:03:14 +0530

    Final commit

---
+++ twitter/app/page.js
@@ -0,0 +1,17 @@
+import Tweet from "./components/Tweet";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <div className="w-full max-w-xl mx-auto border-t border-l border-r border-gray-200">
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+      </div>
+    </main>
+  );
+}
